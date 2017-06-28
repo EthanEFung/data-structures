93a2810c2dae36af0f33debfa894a7acbe064943
Ethan Fung <EthanEFung@gmail.com>
2017-06-28 11:42:11 -0700

    Wrote functional stack.js
    Got passing tests for stacks. Which takes
    a storage and can pop the newest values off
    the storage object.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,33 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    if(size > 0) {
+      var dequeued = storage[0];
+      delete storage[0]
+      size--;
+      return dequeued;
+    }
+   
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
 };
+
+console.log(
+  queue.enqueue('a');
+  queue.enqueue('b');
+  queue.dequeue());
