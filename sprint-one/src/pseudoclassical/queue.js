84f8c58644e2bf1d8976e3aad744dc8473e173f9
Ethan Fung <EthanEFung@gmail.com>
2017-06-29 10:33:19 -0700

    Finish queue.js in pseudoclassical style
    I enjoy this style the most, for the simple reason
    that its the easiest to write. I will have to
    relook at the other styles and figure out the other
    reasons one might utilize them.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,32 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
+
+Queue.prototype.enqueue = function (value) {
+  if (this.storage[this.size()]) {
+    this.storage[this.size() + 1] = value;
+  } else {
+    this.storage[this.size()] = value;  	
+  }
+};
+
+Queue.prototype.dequeue = function () {
+  var dequeued = this.storage[Object.keys(this.storage)[0]];
+  delete this.storage[Object.keys(this.storage)[0]];
+  return dequeued;
+};
+
+Queue.prototype.size = function () {
+  return Object.keys(this.storage).length;
+};
+
+/*
+  List the attributes that are relevent within the queue function.
+    you can use 'this'.
+  set methods onto the prototype of queue
+    (1)enqueue - place a specified value on the end of the queue
+    (2)dequeue - take the first item of the queue off and return value
+    (3)size - return the length of the queue
+*/
 
 
