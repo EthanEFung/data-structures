3e3b7f076644ec53818c1642338e3a038b660abd
Ethan Fung <EthanEFung@gmail.com>
2017-06-29 16:36:02 -0700

    Work through addToTail in linkedlist.js
    Still working through the conception of a linked
    list.  Didn't notice that a LinkedList actually
    creates nodes to house values.  Instead assumed
    you could simply put Node objects into it.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,7 +3,21 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function(node) {
+
+    if (list.head === null) {
+
+      list.head = value;
+      // console.log('This is the head when no head is assigned', list.head);
+
+    } else if (list.tail === null) {
+      list.tail = value;
+      list.head.next = value;
+    } else {
+      list.tail.next = value;
+      list.tail = value;
+      // console.log('This is the head when head is defined', list.head, 'and tail is', list.tail);
+    }
   };
 
   list.removeHead = function() {
@@ -23,6 +37,24 @@ var Node = function(value) {
 
   return node;
 };
+
+var myNode = 10;
+var yourNode = 15;
+var myLinkedList = LinkedList();
+myLinkedList.addToTail(10);
+console.log(myLinkedList);
+
+
+
+// greeter = function() {
+//   var greeting = 'HI'
+//   return greeting
+// }
+
+// console.log(greeter())
+
+// var a = greeter()
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
