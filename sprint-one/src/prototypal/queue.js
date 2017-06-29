84ccdb3cf9fa47cd206cb800da235689a84f2d24
Ethan Fung <EthanEFung@gmail.com>
2017-06-29 09:57:19 -0700

    Wrote a prototypal instantiation of queue
    With the help of Alex Kim, was able create a
    queue without the extra properities of 'first'
    and 'last' appended as properties of the object.
    This saves on memory. This is accomplished by
    utilizing the Object.keys() method to determine
    size, and with Object.keys searching for the first
    item in the array it is referring to.
    
    Another thing to note is that prototypes allow for
    the objects of Queue to contain less properties,
    and therefore are 'lighter'.  Instead, they have
    access to the properties of another object. My mind
    is blown. So cool.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  //attributes here
+
+  return newQueue;
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function(value) {
+  if (this.storage[this.size()]) {
+    this.storage[this.size() + 1] = value;
+  } else {
+    this.storage[this.size()] = value;
+  }
+};
 
+queueMethods.dequeue = function() {
+  var dequeued = this.storage[Object.keys(this.storage)[0]];
+  delete this.storage[Object.keys(this.storage)[0]];
+  return dequeued;
+};
 
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
+
+//create a queueMethods Object that will house
+//(1) enqueue - place one item on the end of the queue
+//(2) dequeue - take the first item of the queue off 
+//(3) size - returns the size or the length of the queue
+
+//Set the queueMethods object as the prototype of the 
+//  function Queue
+//declare any relevent attributes to the queue object
+//return queue Object
