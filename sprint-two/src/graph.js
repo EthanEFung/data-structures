d2ea4f433cda5576337c7a85b3cca58e4c067850
Ethan Fung <EthanEFung@gmail.com>
2017-06-30 11:28:08 -0700

    Wrote operable graph.js
    Discussed with Zach and Hugo a minor bug
    that somehow implements prototype methods
    before console logging. Will have to look
    into the reasons for this behavior.
    
    Also discussed with Hugo and Zach my use
    of _.score in the forEachNode Method on
    lines 43, and 44.  Discussed the prospect
    of changing the tests if the implementation
    is has sound logic and reason. Both gave
    pros and cons.
    
    Pros: We are not bound to the tests, and
    TDD is subject to the dev as a tool to
    prevent consequences.  It is a contract
    and, on occasion if the contract is unfair,
    it is perfectly reasonable to change the
    terms of the contract.
    
    However, granted that most software
    development is built in teams, the cost
    of changing a test when not communicated
    well or not fully reasonable can result
    in detrimental and costly consequences
    down the line for you and other devs.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,37 +1,68 @@
 
-
 // Instantiate a new graph
 var Graph = function() {
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this[node] = {};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.hasOwnProperty(node);
 };
 
-// Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  this.forEachNode(function(item) {
+    if (item.hasOwnProperty(node)) {
+      delete item[node];
+    }
+  });
+  delete this[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this[fromNode].hasOwnProperty(toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this[fromNode][toNode] = true;
+  this[toNode][fromNode] = true;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this[fromNode][toNode];
+  delete this[toNode][fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  _.each(this, cb);
+  //iterate through a node, and for each property call the cb
 };
+// var myGraph = new Graph();
 
+// // debugger;
+// myGraph.addNode(4);
+// console.log(myGraph, ' after adding 4');
+
+// myGraph.addNode(5);
+// console.log(myGraph, ' after adding 5');
+
+// myGraph.addEdge(5, 4);
+// console.log(myGraph, ' after adding edge on 4 and 5');
+
+
+
+// myGraph.removeEdge(5,4);
+// console.log(myGraph, ' before removal of edge');
+
+
+// console.log(myGraph, ' after removal');
 /*
  * Complexity: What is the time complexity of the above functions?
  */
