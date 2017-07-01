35900a384747487a9b0cdb904822e8ec49243e94
Ethan Fung <EthanEFung@gmail.com>
2017-07-01 10:38:50 -0700

    Wrote operating binarySearchTree.insert
    Utilized helper functions and stubbing to
    understand the logic of my function.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,48 @@
 var BinarySearchTree = function(value) {
+  var tree = {};
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+
+  _.extend(tree, searchMethods);
+  return tree;
 };
+
+var searchMethods = {};
+
+searchMethods.insert = function(value) {
+
+  var newTree = BinarySearchTree(value);
+
+  var defineTreeAsLeftOrRight = function(parent, child) {
+    return child.value < parent.value ? 'left' : 'right';
+  };
+
+  var isTheDirectionNull = function(parent, direction) {
+    return parent[direction] === null;
+  };
+
+  var assignTheChildToTheDirection = function(parent, child, direction) {
+    parent[direction] = child;
+  };
+  
+  var insertTree = function (parent, child) {
+    var direction = defineTreeAsLeftOrRight(parent, child);
+
+    if(isTheDirectionNull(parent, direction)) {
+      assignTheChildToTheDirection(parent, child, direction);
+    } else {
+      insertTree(parent[direction], child);
+    }
+  };
+  
+  insertTree(this, newTree);
+};
+
+searchMethods.contains = function () {};
+
+searchMethods.depthFirstLog = function () {};
+
 
 
 /*
