945dfa1afb8ce24e16c181f124a888f9293c41f5
Ethan Fung <EthanEFung@gmail.com>
2017-07-01 11:16:13 -0700

    Write operable binarySearchTree .contains
    Recursed on the .contains function itself.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,3 +1,16 @@
+
+var defineTreeAsLeftOrRight = function(parent, child) {
+  return child.value < parent.value ? 'left' : 'right';
+};
+
+var isTheDirectionNull = function(parent, direction) {
+  return parent[direction] === null;
+};
+
+var assignTheChildToTheDirection = function(parent, child, direction) {
+  parent[direction] = child;
+};
+
 var BinarySearchTree = function(value) {
   var tree = {};
   tree.value = value;
@@ -14,22 +27,12 @@ searchMethods.insert = function(value) {
 
   var newTree = BinarySearchTree(value);
 
-  var defineTreeAsLeftOrRight = function(parent, child) {
-    return child.value < parent.value ? 'left' : 'right';
-  };
 
-  var isTheDirectionNull = function(parent, direction) {
-    return parent[direction] === null;
-  };
-
-  var assignTheChildToTheDirection = function(parent, child, direction) {
-    parent[direction] = child;
-  };
   
   var insertTree = function (parent, child) {
     var direction = defineTreeAsLeftOrRight(parent, child);
 
-    if(isTheDirectionNull(parent, direction)) {
+    if (isTheDirectionNull(parent, direction)) {
       assignTheChildToTheDirection(parent, child, direction);
     } else {
       insertTree(parent[direction], child);
@@ -39,7 +42,30 @@ searchMethods.insert = function(value) {
   insertTree(this, newTree);
 };
 
-searchMethods.contains = function () {};
+searchMethods.contains = function (val) {
+  var determineTheValueLeftOrRight = function(treeValue, val) {
+    return treeValue > val ? 'left' : 'right';
+  };  
+
+  if (this.value === val) {
+    return true;
+  } else {
+
+    var direction = determineTheValueLeftOrRight(this.value, val);
+    console.log('val ', val, ' for this', this, ' is Null ', isTheDirectionNull(this, direction));
+    if (isTheDirectionNull(this, direction)) {
+      return false;
+    } else {
+      return this[direction].contains(val);
+    }
+  }
+  //determine if the current tree has the value
+  //else
+  //determine the values direction
+  //if the direction is null return false
+  //else 
+  //run contains on the direction 
+};
 
 searchMethods.depthFirstLog = function () {};
 
